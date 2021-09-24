996b90249a828b2ad8c4b7354b812888f2a9cf3a
starseed2021 <tiff.harper18@gmail.com>
2021-09-24 13:57:10 -0400

    Added 2 New Tests; Passed Tests

--- tests/test_student.py
+++ tests/test_student.py
@@ -4,3 +4,19 @@ def test_student_class_name_is_correct():
   student = Student("Trenisha", "senior", ["Calculus", "Choir", "Photography", "AP History"])
   # assert
   assert student.name == "Trenisha"
+
+  def test_add_class_adds_class():
+    student = Student("Trenisha", "senior", ["Calculus", "Choir", "Photography", "AP History"])
+
+    adds_class = Student.classes.append('Ceramics')
+    assert student.classes == ["Calculus", "Choir", "Photography", "AP History", "Ceramics"]
+
+
+  def test_length_is_correct():
+
+   student = Student("Trenisha", "senior", ["Calculus", "Choir", "Photography", "AP History"])
+
+   list_length = student.get_num_classes()
+   assert list_length == 4
+
+   
