e29f54129031a4b25016cdb3bcc1f345bcde4b07
starseed2021 <tiff.harper18@gmail.com>
2021-09-24 14:14:39 -0400

    Create 2 New Tests; Passed All

--- tests/test_student.py
+++ tests/test_student.py
@@ -1,22 +1,22 @@
 from school_schedule.student import Student
+
 def test_student_class_name_is_correct():
   # act
   student = Student("Trenisha", "senior", ["Calculus", "Choir", "Photography", "AP History"])
   # assert
   assert student.name == "Trenisha"
 
-  def test_add_class_adds_class():
-    student = Student("Trenisha", "senior", ["Calculus", "Choir", "Photography", "AP History"])
+def test_add_class_adds_class():
+  student = Student("Trenisha", "senior", ["Calculus", "Choir", "Photography", "AP History"])
+  
+  student.classes.append("Ceramics")
+  assert student.classes == ["Calculus", "Choir", "Photography", "AP History", "Ceramics"]
 
-    adds_class = Student.classes.append('Ceramics')
-    assert student.classes == ["Calculus", "Choir", "Photography", "AP History", "Ceramics"]
+def test_length_is_correct():
+
+  student = Student("Trenisha", "senior", ["Calculus", "Choir", "Photography", "AP History"])
+
+  list_length = student.get_num_classes()
+  assert list_length == 4
 
 
-  def test_length_is_correct():
-
-   student = Student("Trenisha", "senior", ["Calculus", "Choir", "Photography", "AP History"])
-
-   list_length = student.get_num_classes()
-   assert list_length == 4
-
-   
